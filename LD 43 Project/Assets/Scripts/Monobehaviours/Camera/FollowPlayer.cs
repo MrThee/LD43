ac466b5bd4aec8aef763a3f31066e7d1e2f1e4e5
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-01 10:12:22 -0800

    Add quick camera following the player

---
+++ LD 43 Project/Assets/Scripts/Monobehaviours/Camera/FollowPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform kPlayer;
+
+    /// <summary>
+    /// Distance from the player to the camera along the direction the camera is facing.
+    /// </summary>
+    public float distanceToPlayer;
+
+    /// <summary>
+    /// How fast it moves to the desired position.
+    /// </summary>
+    public float speediness = 0.1f;
+
+	// Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 desiredPosition = kPlayer.position - distanceToPlayer * transform.forward;
+        Vector3 newPosition = Vector3.Lerp(transform.position, desiredPosition, 0.1f);
+        transform.position = newPosition;
+	}
+}
