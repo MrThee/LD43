78a9ea182b93748b6d7fe60a56b6974aa81b2dab
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-02 15:56:59 -0800

    Beginning to add logic for getting powerups

---
+++ LD 43 Project/Assets/Scripts/Monobehaviours/Cutscenes/Chooser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chooser : MonoBehaviour {
+
+    public RectTransform[] options;
+    public RectTransform selectArrow;
+
+    public int selectedIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // TODO: Use the button things for this?
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
+            selectedIndex--;
+            if (selectedIndex < 0) {
+                selectedIndex += options.Length;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
+            selectedIndex++;
+            selectedIndex %= options.Length;
+        }
+
+        RectTransform selectedOption = options[selectedIndex];
+        Vector3 pos = selectArrow.position;
+        pos.x = selectedOption.position.x;
+
+        selectArrow.position = pos;
+	}
+}
