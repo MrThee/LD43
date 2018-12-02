6046304f44fa039bef246f63fa51fda9c896d075
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-01 21:01:28 -0800

    Start adding cutscene things

---
+++ LD 43 Project/Assets/Scripts/Monobehaviours/CutsceneHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneHandler : MonoBehaviour
+{
+
+    private int actionIndex = -1;
+    private List<Action> cutsceneActions;
+
+    // Use this for initialization
+    void Start()
+    {
+        cutsceneActions = new List<Action>{
+            () => {
+                Debug.Log("Hello");
+            },
+            () => {
+                Debug.Log("Hello 2");
+            },
+            () => {
+                Debug.Log("Hey there 3");
+            }
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            actionIndex++;
+            actionIndex %= cutsceneActions.Count;
+
+            Action action = cutsceneActions[actionIndex];
+            action.Invoke();
+        }
+    }
+}
