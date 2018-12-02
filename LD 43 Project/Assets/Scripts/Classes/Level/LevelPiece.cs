8733f4a2484f96842bf8145bd938ea1299c8edb5
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-01 18:18:04 -0800

    Try add mountains but I don't like it lol

---
+++ LD 43 Project/Assets/Scripts/Classes/Level/LevelPiece.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Represents a piece of the level map, but not actually tied to a specific game object
+/// </summary>
+public class LevelPiece
+{
+    public enum EdgeType
+    {
+        Field
+    }
+
+    public struct Edge {
+        public EdgeType type;
+        public Vector2 dir;
+    }
+
+    public bool[,] map;
+    public List<EdgeType> edges;
+
+    public LevelPiece()
+    {
+    }
+}
---
+++ LD 43 Project/Assets/Scripts/Monobehaviours/Environment/MountainGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountainGenerator : MonoBehaviour {
+
+    public Transform[] mountains;
+    public Vector3 spawnRange;
+    public int numMountains;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < numMountains; i++)
+        {
+            Transform mountainType = mountains[Random.Range(0, mountains.Length)];
+
+            Vector3 pos = new Vector3();
+            pos.x = Random.Range(-spawnRange.x, spawnRange.x);
+            pos.y = Random.Range(-spawnRange.y, spawnRange.y);
+            pos.z = Random.Range(-spawnRange.z, spawnRange.z);
+
+            Transform mountain = Instantiate(mountainType, transform.position + pos, Quaternion.identity);
+        }
+		
+	}
+	
+}
