2d62c1be481bf4fc79c3fa9a6935b4b7cd30f595
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-02 23:40:09 -0800

    Health UI

---
+++ LD 43 Project/Assets/Scripts/Monobehaviours/UI/HealthMeter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthMeter : MonoBehaviour {
+
+    public RectTransform HeartPrefab;
+    public float heartSpacing = 20f;
+
+    private List<RectTransform> hearts;
+
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+
+        hearts = new List<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // need to add more
+        while (player.hp > hearts.Count) {
+            RectTransform heart = Instantiate(HeartPrefab, transform);
+            heart.localPosition = hearts.Count * heartSpacing * Vector3.right;
+            hearts.Add(heart);
+        }
+
+        while (player.hp < hearts.Count && hearts.Count > 0) {
+            // need to remove some
+            RectTransform heart = hearts[hearts.Count - 1];
+            Destroy(heart.gameObject);
+            hearts.RemoveAt(hearts.Count - 1);
+        }
+	}
+}
