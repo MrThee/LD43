055a46fcfd57460fb5c9b733747c545b96a63e50
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-02 22:30:03 -0800

    Add some SFX and split up the level again

--- LD 43 Project/Assets/Scripts/Coin.cs
+++ LD 43 Project/Assets/Scripts/Coin.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour {
+    
+    public AudioClip sound;
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(this.gameObject);
+        PlayerController player = other.GetComponent<PlayerController>();
+        if (player == null) {
+            return;
+        }
+
+        Destroy(gameObject);
         // TODO: Get a coin
-        // TODO: Coin SFX
+        AudioSource.PlayClipAtPoint(sound, this.transform.position);
     }
 }
