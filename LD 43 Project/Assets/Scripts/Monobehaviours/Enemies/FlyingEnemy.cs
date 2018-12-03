f3ef083ca095f42370e060758b91be24e547c5fc
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-03 00:21:31 -0800

    FLying enemies only attack if you're near

--- LD 43 Project/Assets/Scripts/Monobehaviours/Enemies/FlyingEnemy.cs
+++ LD 43 Project/Assets/Scripts/Monobehaviours/Enemies/FlyingEnemy.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class FlyingEnemy : AIController {
     
+    public enum State
+    {
+        Idle,
+        Attack
+    }
     private Rigidbody body;
+
+    public State state { get; private set; }
+    private System.Action<float> m_stateAction;
 
     public float speed = 1f;
     private Vector3 velocity = new Vector3();
@@ -13,9 +21,34 @@ public class FlyingEnemy : AIController {
 	protected override void Start(){
         base.Start();
         body = GetComponent<Rigidbody>();
+
+        this.ChangeState(Idle, State.Idle);
     }
+
+    void FixedUpdate()
+    {
+        float deltaTime = Time.deltaTime;
+        m_stateAction.Invoke(deltaTime);
+        kCharacter.UpdateState(deltaTime);
+    }
+
+    void ChangeState(System.Action<float> callback, State state)
+    {
+        this.state = state;
+        m_stateAction = callback;
+    }
+
+    void Idle(float deltaTime)
+    {
+        Vector3 toPlayer = GetToPlayer();
+        if (toPlayer.magnitude < 7f)
+        {
+            ChangeState(Attack, State.Attack);
+        }
+    }
+
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Attack (float deltaTime) {
         Vector3 direction = (player.transform.position - transform.position).normalized;
         Vector3 desiredVelocity = speed * direction;
         velocity = Vector3.Slerp(velocity, desiredVelocity, 0.1f);
