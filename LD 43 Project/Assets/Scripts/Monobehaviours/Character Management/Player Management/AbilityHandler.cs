98e150b7ec7e648ba501ecda41036364b125398d
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-02 19:07:20 -0800

    Start adding ability things

---
+++ LD 43 Project/Assets/Scripts/Monobehaviours/Character Management/Player Management/AbilityHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityHandler : MonoBehaviour {
+
+    public enum Ability {
+        Dash,
+        Gun,
+        Butterfly,
+        Walljump
+    }
+
+    public List<Ability> OwnedAbilities { get; private set; }
+
+    public ButterflyFriend butterflyPrefab;
+
+    private PlayerController player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+
+        OwnedAbilities = new List<Ability>();
+        GrantAbility(Ability.Dash);
+        GrantAbility(Ability.Gun);
+        GrantAbility(Ability.Butterfly);
+    }
+
+    public bool HasAbility(Ability ability) {
+        return OwnedAbilities.Contains(ability);
+    }
+
+    public void GrantAbility(Ability ability)
+    {
+        OwnedAbilities.Remove(ability);
+
+        // If needed, logic for adding the thing
+        switch (ability) {
+            case Ability.Butterfly:
+                ButterflyFriend butterfly = Instantiate(butterflyPrefab);
+                butterfly.transform.position = player.transform.position;
+                break;
+        }
+    }
+
+    public void RemoveAbility(Ability ability) {
+        if (!HasAbility(ability)) {
+            return;
+        }
+        OwnedAbilities.Remove(ability);
+
+        // If needed, logic for removing the thing
+        switch (ability)
+        {
+            case Ability.Butterfly:
+                ButterflyFriend butterfly = FindObjectOfType<ButterflyFriend>();
+                // TODO: butterfly dying VFX/SFX
+                Destroy(butterfly.gameObject);
+                break;
+        }
+    }
+}
