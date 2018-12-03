931c321e87ac52d976c33481723003be08b0bfcd
Phillip Woytowitz <phwoytowitz@gmail.com>
2018-12-03 00:36:41 -0800

    Fidget Spinners now spin

---
+++ LD 43 Project/Assets/Prefabs/Facial Feature Prefabs/Fidget Spinners/SpinOnAxis.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinOnAxis : MonoBehaviour {
+
+	public Vector3 axis = Vector3.forward;
+	public float omega = 720f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(axis, omega*Time.deltaTime);
+	}
+}
