16a216ddc4803bf05ac44147b3a0a9bc31b5409b
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-03 02:43:09 -0800

    Add more of a cutscene and hopefully give the player ability

--- LD 43 Project/Assets/Scripts/Monobehaviours/Characters/PowerupMan.cs
+++ LD 43 Project/Assets/Scripts/Monobehaviours/Characters/PowerupMan.cs
@@ -34,26 +34,79 @@ public class PowerupMan : MonoBehaviour {
         cutsceneHandler.StartCutscene(new List<System.Action>
         {
             () => {
-                camera.Focus(transform);
+                camera.FocusOnPlayer();
                 camera.config.distanceToFocus = 10;
                 camera.config.focusOffset = 0;
-                cutsceneHandler.SetSpeech("Hello friend");
+                cutsceneHandler.SetSpeech("Hello!");
+            },
+            () => {
+                camera.Focus(transform);
+                cutsceneHandler.SetSpeech("It's dangerous to go alone! Take this!");
             },
             () => {
                 camera.FocusOnPlayer();
-                cutsceneHandler.SetSpeech("Hi there");
+                cutsceneHandler.SetSpeech("...");
             },
             () => {
                 camera.Focus(transform);
+                cutsceneHandler.SetSpeech("At this altar, the worthy receive a gift from The Gourd.");
+            },
+            () => {
+                cutsceneHandler.SetSpeech("This weapon will let you defend against the rotten produce of this land.");
+            },
+            () => {
+                camera.FocusOnPlayer();
+                cutsceneHandler.SetSpeech("Great!");
+            },
+            () => {
+                camera.Focus(transform);
+                camera.config.distanceToFocus = 7;
+                cutsceneHandler.SetSpeech("Hold on!");
+            },
+            () => {
+                cutsceneHandler.SetSpeech("Only the worthy receive the gift.");
+            },
+            () => {
+                cutsceneHandler.SetSpeech("To prove your worth...");
+            },
+            () => {
+                camera.config.distanceToFocus = 6;
+                cutsceneHandler.SetSpeech("You must...");
+            },
+            () => {
+                camera.config.distanceToFocus = 5;
+                cutsceneHandler.SetSpeech("Sacrifice!");
+            },
+            () => {
+                cutsceneHandler.SetSpeech("A sacrifice must be made of one of your abilities.");
+            },
+            () => {
+                cutsceneHandler.SetSpeech("Choose wisely, for you shall not receive it again.");
+            },
+            () => {
+                camera.config.distanceToFocus = 10;
+                camera.FocusOnPlayer();
                 cutsceneHandler.ShowChoice();
             },
             () => {
                 cutsceneHandler.HideChoice();
                 camera.Focus(transform);
+                AbilityHandler.Ability selectedAbility = cutsceneHandler.chooser.SelectedAbility;
                 cutsceneHandler.SetSpeech(
                     string.Format(
-                        "You picked item {0}. Thanks.",
-                        cutsceneHandler.chooser.selectedIndex + 1));
+                        "Sacrificing {0}. A wise choice.",
+                        selectedAbility.ToString()));
+                
+                AbilityHandler handler = FindObjectOfType<AbilityHandler>();
+                handler.RemoveAbility(selectedAbility);
+
+                handler.GrantAbility(AbilityHandler.Ability.Gun);
+            },
+            () => {
+                cutsceneHandler.SetSpeech("Now behold! The peashooter!");
+            },
+            () => {
+                cutsceneHandler.SetSpeech("Click to fire your weapon!");
             },
             () => {
                 shownCutscene = true;
