df9b63cc449fb04ec29463dc8bc3d6f365d656e9
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-12-03 19:17:37 -0800

    ADD THE BUTTONS TO SAY WHO WE ARE

---
+++ LD 43 Project/Assets/Scripts/GoToJezzamon.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToJezzamon : MonoBehaviour {
+
+    public void OpenUrl() {
+        Application.OpenURL("https://twitter.com/jezzamonn");
+    }
+}
---
+++ LD 43 Project/Assets/Scripts/GoToMrThee.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToMrThee : MonoBehaviour {
+
+    public void OpenUrl()
+    {
+        Application.OpenURL("https://twitter.com/PhillipWitz");
+    }
+}
